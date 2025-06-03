ee615a8551f00e0c60b2d6777a6a5cdc7abafede
Vijaypratapsingh <vijaypratap1747singh@gmail.com>
2025-06-03 20:11:47 +0530

    lat

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { Server } from "socket.io";
 const io = new Server({
   cors: {
     //origin: ""http://localhost:5173""
-    origin: "*",
+    origin: "https://client-tau-green.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
